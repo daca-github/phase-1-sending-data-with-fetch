d28b610d2e9ad54834da7c3638e27a6e227319fd
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-04-26 14:32:45 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({name, email})
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML = id;
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message;
+      });
+  }
+  
